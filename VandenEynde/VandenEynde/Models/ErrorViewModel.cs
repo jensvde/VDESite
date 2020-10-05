0113dbca3bc6b0bf97b7985d6801a312c5877a12
jensvde <jens.vandeneynde@telenet.be>
2020-10-05 23:17:04 +0200

    Default
    Default empty project

---
+++ VandenEynde/VandenEynde/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VandenEynde.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
